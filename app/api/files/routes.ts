2ec457abd79d76ac0b83020b1cf01535df7c8bdb
Joao <joaovictor.go.ro@gmail.com>
2024-11-27 10:21:57 -0400

    remove unused code

--- app/api/files/routes.ts
+++ app/api/files/routes.ts
@@ -6,7 +6,6 @@ import { CSVLoader } from 'api/csv';
 import entities from 'api/entities';
 import { processDocument } from 'api/files/processDocument';
 import { uploadMiddleware } from 'api/files/uploadMiddleware';
-import { legacyLogger } from 'api/log';
 import { permissionsContext } from 'api/permissions/permissionsContext';
 import { validateAndCoerceRequest } from 'api/utils/validateRequest';
 import { EntitySchema } from 'shared/types/entityType';
