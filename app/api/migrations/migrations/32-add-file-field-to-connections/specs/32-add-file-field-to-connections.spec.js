43da63c82d00a61e55884ca85d2dfe61c42ee8ef
Joao <joaovictor.go.ro@gmail.com>
2024-12-03 08:37:07 -0400

    fix flaky test

--- app/api/migrations/migrations/32-add-file-field-to-connections/specs/32-add-file-field-to-connections.spec.js
+++ app/api/migrations/migrations/32-add-file-field-to-connections/specs/32-add-file-field-to-connections.spec.js
@@ -36,12 +36,9 @@ describe('migration add-file-field-to-connections', () => {
   });
 
   it('should work with unsuported languages by elastic without errors', async () => {
-    fixtures.settings[0].languages = [
-      {
-        key: 'ja',
-        default: true,
-      },
-    ];
+    const fullyUnsupportedElasticLanguage = { key: 'kn', default: true };
+    fixtures.settings[0].languages = [fullyUnsupportedElasticLanguage];
+
     await testingDB.clearAllAndLoad(fixtures);
     await migration.up(testingDB.mongodb);
   });
