32fc6f8c500efed4dae4ed142748307987d7773f
John <borrajosantiago@gmail.com>
2024-12-05 12:13:19 -0300

    update spec

--- app/react/V2/Components/PDFViewer/functions/specs/calculateScaling.spec.ts
+++ app/react/V2/Components/PDFViewer/functions/specs/calculateScaling.spec.ts
@@ -8,31 +8,23 @@ describe('pdf scaling calculation', () => {
     });
 
     it('should return the scaling if the width ratio is above 1', () => {
-      const result = calculateScaling(1.8, 15, 20);
-      expect(result).toEqual(0.7407407407407407);
+      const result = calculateScaling(3, 450, 500);
+      expect(result).toEqual(0.5);
     });
   });
 
   describe('device scaling is less than 1', () => {
     it('should return a ratio based on the device scaling', () => {
-      const result = calculateScaling(0.7, 12, 24);
-      expect(result).toEqual(1.4);
-    });
-
-    it('should return the same scaling for device pixel ratios below .5', () => {
-      let result = calculateScaling(0.4, 12, 30);
-      expect(result).toEqual(1.25);
-
-      result = calculateScaling(0.2, 12, 30);
+      const result = calculateScaling(0.6, 12, 24);
       expect(result).toEqual(1.25);
     });
   });
 
   it('should have a minimun scaling', () => {
     let result = calculateScaling(0.4, 50, 10);
-    expect(result).toEqual(0.5);
+    expect(result).toEqual(0.8);
 
     result = calculateScaling(0.4, 500, 10);
-    expect(result).toEqual(0.5);
+    expect(result).toEqual(0.8);
   });
 });
