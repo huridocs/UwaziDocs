0a340cdb9a21fcb233b47fad93ac9475649f4bc3
John <borrajosantiago@gmail.com>
2025-01-12 12:04:54 -0300

    fix export

--- app/react/I18N/index.js
+++ app/react/I18N/index.js
@@ -1,12 +1,12 @@
 import I18NLink from './components/I18NLink';
 import I18N from './components/I18N';
 import I18NUtils from './utils';
-import I18NApi from './I18NApi';
 import * as actions from './actions/I18NActions';
 
-export { I18NLink, I18NUtils, I18N, I18NApi, actions };
+export * as I18NApi from './I18NApi';
 export { I18NMenu } from './components/I18NMenu';
 export { I18NLink as I18NLinkV2 } from './I18NLinkV2';
 export { Translate } from './Translate';
 export { t } from './translateFunction';
 export { TranslateModal } from './TranslateModal';
+export { I18NLink, I18NUtils, I18N, actions };
