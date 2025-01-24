6fc9a2be3a6900d33a2acb1ead1b20bf5a7d247c
Joao <joaovictor.go.ro@gmail.com>
2025-01-24 16:53:06 -0400

    feat: add serverError, clientError, and jsonResponse methods to AbstractController

--- app/api/common.v2/AbstractController.ts
+++ app/api/common.v2/AbstractController.ts
@@ -16,4 +16,23 @@ export abstract class AbstractController {
   }
 
   abstract handle(request: Request): Promise<void>;
+
+  serverError(error: Error) {
+    // Logging ?
+
+    return this.res.status(500).json({
+      message: error.message,
+    });
+  }
+
+  clientError(message: string) {
+    // Should we log this ?
+    // What about negative impacts spam on Notifications channel ?
+    return this.res.status(400).json({ message });
+  }
+
+  jsonResponse(body: any) {
+    this.res.status(200).json(body);
+    this.next();
+  }
 }
