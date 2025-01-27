28afd57d8beebc98bbaf6d84597cc7e6d818769a
Joao <joaovictor.go.ro@gmail.com>
2025-01-27 15:52:08 -0400

    Refactor DomainObject class to remove unused event handling methods

--- app/api/common.v2/domain/DomainObject.ts
+++ app/api/common.v2/domain/DomainObject.ts
@@ -1,21 +1,7 @@
-import { Event } from './Event';
 import { IdentifiedDomainObject, IdentifiedDomainObjectProps } from './IdentifiedDomainObject';
 
 type DomainObjectProps = IdentifiedDomainObjectProps;
 
-export abstract class DomainObject extends IdentifiedDomainObject {
-  private events: Event[] = [];
-
-  protected addEvent(event: Event): void {
-    this.events.push(event);
-  }
-
-  releaseEvents() {
-    const events = [...this.events];
-    this.events = [];
-
-    return events;
-  }
-}
+export abstract class DomainObject extends IdentifiedDomainObject {}
 
 export type { DomainObjectProps };
